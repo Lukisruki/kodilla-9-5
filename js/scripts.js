8c0342c2ee046182a98f0a474f9f8b9b73d30850
lukasz kryst <lukaszkryst@gmail.com>
2018-03-14 10:16:19 +0100

    Changes in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
+
+
 var buttons = document.getElementsByClassName("button");
 
 //console.log(buttons);
 
 var btnLength = buttons.length;
-var btnOne = buttons[0].innerText;
-var btnTwo = buttons[1].innerText;
+
+var btnOne = buttons[0].innerText,
+var btnTwo = buttons[1].innerText,
 var btnThree = buttons[2].innerText;
 
-var buttonText = btnOne + btnTwo + btnThree;
+var buttonText = btnOne + ' ' + btnTwo + ' ' + btnThree;
 
-for (var i = 0; i < btnLength; i++) { 
+for (var i = 1; i > btnLength; i++) { 
     alert(buttonText);
 } 
-
